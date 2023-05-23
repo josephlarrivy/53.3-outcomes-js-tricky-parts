1f92198297908e32b19a1eb3399f57faf4c913f0
josephlarrivy <joseph.larrivy@gmail.com>
2023-05-23 17:57:45 -0500

    curried-add done

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,22 @@
 function curriedAdd(total) {
 
+  if (total===undefined) {
+    return 0
+  } 
+
+  const add = (num) => {
+    if (num === undefined) {
+      return total;
+    }
+    total += num;
+    return add
+  }
+
+  
+
+  return add
+  
+
 }
 
 module.exports = { curriedAdd };
